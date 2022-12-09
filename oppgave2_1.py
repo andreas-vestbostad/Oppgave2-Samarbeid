7cef4bd4e85e4fcb961232f3e91541a125e6448d
megadummitick <71771111+megadummitick@users.noreply.github.com>
2022-12-09 09:51:34 +0100

    Kommenterer bedre programmet

--- oppgave2_1.py
+++ oppgave2_1.py
@@ -1,8 +1,10 @@
+#Importerer nødvendige bibliotek
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 
+#Finner filen ved hjelp av pathlib
 filnavn = Path(__file__).parent / "Befolkning.csv"
 
 # Lister for å ta vare på alle årstall og befolkningsstørrelser
@@ -12,9 +14,12 @@ befolkning = []
 # Åpner fil og legger til verdier i hver liste
 with open(filnavn, encoding="utf-8-sig") as fil:
     filinnhold = csv.reader(fil, delimiter=";")
+    #Hopper over første linje i filen og legger til linjen i variabelen "tittel"
     tittel = next(filinnhold)
+    #Hopper over andre linje i filen og legger til linjen i variabelen "overskrifter"
     overskrifter = next(filinnhold)
-  
+    
+    #Loop som går gjennom hver line i filen og legger den første kolonnen til i "aarstall", og den andre kollenen til i "befolkning"
     for rad in filinnhold:
         aarstall.append(int(rad[0]))
         befolkning.append(int(rad[1]))
